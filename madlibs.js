037eb7c1b554103c3bd1851d25faaa2703d2509d
Glitch (lily-ruddy-ticket) <none>
2021-05-24 03:06:31 +0000

    👢🚴 Checkpoint ./madlibs.js:3856783/606

--- madlibs.js
+++ madlibs.js
@@ -1,11 +1,17 @@
 // An example!
 let exampleNoun = prompt("Please give me a plural noun (person, place, or thing)");
-console.log("One incredible morning, Jourdan, queen of the " + exampleNoun + " woke up and made a decree.")
+
+console.log(`One incredible morning, Jourdan, queen of the ${exampleNoun} woke up and made a decree.")
+
 console.log("'Starting tomorrow,' she said, 'all " + exampleNoun + " will need to be transformed into puppies.'")
-console.log("Jourdan went down in history as the best queen of all time.")
+
+console.log("Jourdan went down in history as the best queen of all time.`)
 
 // Initialize your variables here.
-
+let adjective1 = prompt("Please give me an adjective");
+let verb1 = prompt("Please give me a 3");
+let noun2 = prompt("Please give me a plural noun (person, place, or thing)");
+let adjective2 = prompt("Please give me a plural noun (person, place, or thing)");
 
 
 
